9f8a6481a82bef079d8e4ba162e3994d30565249
aisyahsohimi <77560946+aisyahsohimi@users.noreply.github.com>
2021-01-17 22:22:11 +0800

    Update MapGenerator.java

--- MapGenerator.java
+++ MapGenerator.java
@@ -4,14 +4,18 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-public class MapGenerator {
+public class MapGenerator 
+{
 	public int map[][];
 	public int brickWidth;
 	public int brickHeight;
-	public MapGenerator(int row, int col) {
+	public MapGenerator(int row, int col) 
+	{
 		map = new int[row][col];
-		for(int i = 0; i < map.length; i++) {
-			for(int j = 0; j < map[0].length; j++) {
+		for(int i = 0; i < map.length; i++)
+		{
+			for(int j = 0; j < map[0].length; j++) 
+			{
 				map[i][j] = 1; 
 			}
 		}
@@ -20,24 +24,27 @@ public class MapGenerator {
 		brickHeight = 150/row;
 	}
 	
-	public void draw(Graphics2D g) {
-		for(int i = 0; i < map.length; i++) {
-			for(int j = 0; j < map[0].length; j++) {
-				if(map[i][j] > 0) {
+	public void draw(Graphics2D g)
+	{
+		for(int i = 0; i < map.length; i++) 
+		{
+			for(int j = 0; j < map[0].length; j++)
+			{
+				if(map[i][j] > 0) 
+				{
 					g.setColor(Color.red); 
 					g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 					
 					g.setStroke(new BasicStroke(3));
 					g.setColor(Color.black);
 					g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
-					
-					
 				}
 			}
 		}
 	}
 	
-	public void setBrickValue(int value, int row, int col) {
+	public void setBrickValue(int value, int row, int col) 
+	{
 		map[row][col] = value;
 	}
 }
